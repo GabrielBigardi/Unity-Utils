c9b74661eb915a45bcdad796c1b2ccf281d389fb
Gabriel Bigardi <42253181+GabrielBigardi@users.noreply.github.com>
2021-11-07 11:51:42 -0300

    Added Sliced Filled Image

---
+++ Scripts/Extensions/Sliced Filled Image/Editor/SlicedFilledImageEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+// Custom Editor to order the variables in the Inspector similar to Image component
+[CustomEditor(typeof(SlicedFilledImage)), CanEditMultipleObjects]
+public class SlicedFilledImageEditor : Editor
+{
+	private SerializedProperty spriteProp, colorProp;
+	private GUIContent spriteLabel;
+
+	private void OnEnable()
+	{
+		spriteProp = serializedObject.FindProperty("m_Sprite");
+		colorProp = serializedObject.FindProperty("m_Color");
+		spriteLabel = new GUIContent("Source Image");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(spriteProp, spriteLabel);
+		EditorGUILayout.PropertyField(colorProp);
+		DrawPropertiesExcluding(serializedObject, "m_Script", "m_Sprite", "m_Color", "m_OnCullStateChanged");
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
+#endif
