ad626ea634cdaeea030aa1c3a1e5bfee3ef95c21
Gabriel Bigardi <42253181+GabrielBigardi@users.noreply.github.com>
2022-03-10 13:18:38 -0300

    Update Parallax.cs

--- Scripts/Parallax/Parallax.cs
+++ Scripts/Parallax/Parallax.cs
@@ -2,30 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum UpdateMode
+{
+    Update,
+    FixedUpdate,
+    LateUpdate
+}
+
 public class Parallax : MonoBehaviour
 {
-    public Camera cam;
-    public Transform subject;
+    [Header("Needed Objects")]
+    [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _subject;
 
-    Vector2 startPosition;
-    float startZ;
+    [Header("Config")]
+    [SerializeField] private UpdateMode _updateMode;
+    [SerializeField] private bool lockY = false;
+    [SerializeField] private bool lockX = false;
+    [SerializeField] [Range(0.1f, 2f)] private float _smoothingFactor = 1;
 
-    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+    private Vector3 _startingPos;
 
-    float distanceFromSubject => transform.position.z - subject.position.z;
-    float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
-    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
+    private float TravelX => this._camera.transform.position.x - this._startingPos.x;
+    private float TravelY => this._camera.transform.position.y - this._startingPos.y;
+
+    private float DistanceFromSubject => this._startingPos.z - this._subject.transform.position.z;
+    private float ClipPlane => this._camera.transform.position.z + (this.DistanceFromSubject > 0 ? this._camera.farClipPlane : -this._camera.nearClipPlane);
+    private float ParallaxFactor => Mathf.Abs(this.DistanceFromSubject) / this.ClipPlane;
+
+    private float NewX => this.lockX ? this._startingPos.x : this._startingPos.x + (this.TravelX * this.ParallaxFactor * this._smoothingFactor);
+    private float NewY => this.lockY ? this._startingPos.y : this._startingPos.y + (this.TravelY * this.ParallaxFactor * this._smoothingFactor);
+
 
     private void Start()
     {
-        startPosition = transform.position;
-        startZ = transform.position.z;
+        this._startingPos = transform.position;
     }
 
     private void Update()
     {
-        Vector2 newPos = startPosition + travel * parallaxFactor;
-        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+        if (_updateMode != UpdateMode.Update) return;
 
+        this.transform.position = new Vector3(this.NewX, this.NewY, this._startingPos.z);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_updateMode != UpdateMode.FixedUpdate) return;
+
+        this.transform.position = new Vector3(this.NewX, this.NewY, this._startingPos.z);
+    }
+
+    private void LateUpdate()
+    {
+        if (_updateMode != UpdateMode.LateUpdate) return;
+
+        this.transform.position = new Vector3(this.NewX, this.NewY, this._startingPos.z);
     }
 }
